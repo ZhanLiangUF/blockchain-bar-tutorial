043ccdb71de25da7e1ce6a350058b4c34369bb69
zhanlianguf <zhanliang1994@gmail.com>
2021-07-26 13:06:12 -0400

    Add state functionality for JSON use only

--- db/state.go
+++ db/state.go
@@ -2,6 +2,8 @@ package db
 
 import (
 	"bufio"
+	"encoding/json"
+	"fmt"
 	"os"
 	"path/filepath"
 )
@@ -38,6 +40,66 @@ func NewStateFromDisk() (*State, error) {
 	state := &State{balances, make([]Tx, 0), f}
 
 	for scanner.Scan() {
+		if err := scanner.Err(); err != nil {
+			return nil, err
+		}
 
+		var tx Tx
+		json.Unmarshal(scanner.Bytes(), &tx)
+
+		if err := state.apply(tx); err != nil {
+			return nil, err
+		}
 	}
+
+	return state, nil
+}
+
+func (s *State) Add(tx Tx) error {
+	if err := s.apply(tx); err != nil {
+		return err
+	}
+
+	s.txMempool = append(s.txMempool, tx)
+
+	return nil
+}
+
+func (s *State) Persist() error {
+	mempool := make([]Tx, len(s.txMempool))
+	copy(mempool, s.txMempool)
+
+	for i := 0; i < len(mempool); i++ {
+		txJson, err := json.Marshal(mempool[i])
+		if err != nil {
+			return err
+		}
+
+		if _, err = s.dbFile.Write(append(txJson, '\n')); err != nil {
+			return err
+		}
+		s.txMempool = s.txMempool[1:]
+	}
+
+	return nil
+}
+
+func (s *State) Close() {
+	s.dbFile.Close()
+}
+
+func (s *State) apply(tx Tx) error {
+	if tx.IsReward() {
+		s.Balances[tx.To] += tx.Value
+		return nil
+	}
+
+	if s.Balances[tx.From] < tx.Value {
+		return fmt.Errorf("insufficient balance")
+	}
+
+	s.Balances[tx.From] -= tx.Value
+	s.Balances[tx.To] += tx.Value
+
+	return nil
 }
